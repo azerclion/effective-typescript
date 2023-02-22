82f090fe3a2d7200a5f6037bdeefb9a4d1aeb960
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-02-22 16:57:34 +0900

    fix 1. to doc /export default{}/

--- ch02/15/08.ts
+++ ch02/15/08.ts
@@ -1,38 +1,42 @@
 function parseCSV(input: string): { [columnName: string]: string }[] {
-  const lines = input.split('\n')
-  const [header, ...rows] = lines
-  return rows.map(rowStr => {
-    const row: { [columnName: string]: string } = {}
-    rowStr.split(',').forEach((cell, i) => {
-      row[header[i]] = cell
-    })
-    return row
-  })
+  const lines = input.split("\n");
+  const [header, ...rows] = lines;
+  return rows.map((rowStr) => {
+    const row: { [columnName: string]: string } = {};
+    rowStr.split(",").forEach((cell, i) => {
+      row[header[i]] = cell;
+    });
+    return row;
+  });
 }
 interface ProductRow {
-  productId: string
-  name: string
-  price: string
+  productId: string;
+  name: string;
+  price: string;
 }
 
-declare let csvData: string
-const products = parseCSV(csvData) as unknown as ProductRow[]
-function safeParseCSV(input: string): { [columnName: string]: string | undefined }[] {
-  return parseCSV(input)
+declare let csvData: string;
+const products = parseCSV(csvData) as unknown as ProductRow[];
+function safeParseCSV(
+  input: string
+): { [columnName: string]: string | undefined }[] {
+  return parseCSV(input);
 }
 interface Row1 {
-  [column: string]: number
+  [column: string]: number;
 } // Too broad
+
 interface Row2 {
-  a: number
-  b?: number
-  c?: number
-  d?: number
+  a: number;
+  b?: number;
+  c?: number;
+  d?: number;
 } // Better
+
 type Row3 =
   | { a: number }
   | { a: number; b: number }
   | { a: number; b: number; c: number }
-  | { a: number; b: number; c: number; d: number }
+  | { a: number; b: number; c: number; d: number };
 
-export default {}
+export default {};
